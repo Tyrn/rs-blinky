8a37503151f66d92ce96614b9b609298950f22e8
Tyrn <orlov@bos-ltd.ru>
2021-12-09 12:37:00 +0300

    Dot and dash defined

--- src/main.rs
+++ src/main.rs
@@ -46,12 +46,14 @@ fn main() -> ! {
     // delay.
     let mut delay = Delay::new(cp.SYST, clocks);
 
+    const DOT_DELAY: u16 = 1_00_u16;
+    const DASH_DELAY: u16 = 1_00_u16;
 
     // Now, enjoy the lightshow!
     loop {
         led.set_high().ok();
-        delay.delay_ms(1_00_u16);
+        delay.delay_ms(DOT_DELAY);
         led.set_low().ok();
-        delay.delay_ms(2_00_u16);
+        delay.delay_ms(DASH_DELAY);
     }
 }
